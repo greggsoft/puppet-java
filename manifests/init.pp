0785ab62884c84f2e011dd6205ea489fedded3f9
Mikhail Menshenin <greggsoft@gmail.com>
2013-06-26 10:15:04 +0600

    package name choosen by os family

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,7 +12,11 @@
 #
 # [Remember: No empty lines between comments and class definition]
 class java {
-	package { "java-1.6.0-openjdk":
+	$package = $osfamily ? {
+		'Debian' => 'default-jdk',
+		default  => 'java-1.6.0-openjdk',
+	}
+	package { "$package":
 		ensure => installed
 	}
 }
